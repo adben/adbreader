689f7a67c3e25f24a23e23b25ded40f558806424
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2012-07-03 15:35:24 +0200

    refactored network usage

---
+++ gen/R.java.d
@@ -0,0 +1,13 @@
+/Users/abenedetti/Documents/workspace/NetworkUsage/gen/com/example/android/networkusage/R.java \
+ : /Users/abenedetti/Documents/workspace/NetworkUsage/res/drawable-hdpi/ic_launcher.png \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/drawable-ldpi/ic_launcher.png \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/drawable-mdpi/ic_launcher.png \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/layout/main.xml \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/menu/mainmenu.xml \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/values/arrays.xml \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/values/strings.xml \
+/Users/abenedetti/Documents/workspace/NetworkUsage/res/xml/preferences.xml \
+/Users/abenedetti/Documents/workspace/NetworkUsage/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/abenedetti/Documents/workspace/NetworkUsage/bin/res/drawable-ldpi/ic_launcher.png \
+/Users/abenedetti/Documents/workspace/NetworkUsage/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/abenedetti/Documents/workspace/NetworkUsage/bin/AndroidManifest.xml \
---
+++ src/com/example/android/networkusage/NetworkReceiver.java
@@ -0,0 +1,73 @@
+package com.example.android.networkusage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+import android.util.Log;
+
+/**
+ * 
+ * This BroadcastReceiver intercepts the
+ * android.net.ConnectivityManager.CONNECTIVITY_ACTION, which indicates a
+ * connection change. It checks whether the type is TYPE_WIFI. If it is, it
+ * checks whether Wi-Fi is connected and sets the wifiConnected flag in the main
+ * activity accordingly.
+ * 
+ */
+public class NetworkReceiver extends BroadcastReceiver {
+	NetworkActivity mNetworkActivity;
+	private static final String DEBUG_TAG = "NetworkReceiver";
+
+	public NetworkReceiver(NetworkActivity networkActivity) {
+		mNetworkActivity = networkActivity;
+	}
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		ConnectivityManager connMgr = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+		// Checks the user prefs and the network connection. Based on the
+		// result, decides
+		// whether
+		// to refresh the display or keep the current display.
+		// If the userpref is Wi-Fi only, checks to see if the device has a
+		// Wi-Fi connection.
+		if (NetworkActivity.WIFI.equals(mNetworkActivity.getsPref())
+				&& networkInfo != null
+				&& networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+			// If device has its Wi-Fi connection, sets refreshDisplay
+			// to true. This causes the display to be refreshed when the user
+			// returns to the app.
+			NetworkActivity.setRefreshDisplay(true);
+			Toast.makeText(context, R.string.wifi_connected, Toast.LENGTH_SHORT)
+					.show();
+			Log.d(DEBUG_TAG, "Devices has its Wifi connnection, refeshDisplay => true");
+
+			// If the setting is ANY network and there is a network connection
+			// (which by process of elimination would be mobile), sets
+			// refreshDisplay to true.
+		} else if (NetworkActivity.ANY.equals(mNetworkActivity.getsPref())
+				&& networkInfo != null) {
+			NetworkActivity.setRefreshDisplay(true);
+			Log.d(DEBUG_TAG, "Devices has another connnection, refeshDisplay => true");
+
+			// Otherwise, the app can't download content--either because there
+			// is no network
+			// connection (mobile or Wi-Fi), or because the pref setting is
+			// WIFI, and there
+			// is no Wi-Fi connection.
+			// Sets refreshDisplay to false.
+		} else {
+			NetworkActivity.setRefreshDisplay(false);
+			Toast.makeText(context, R.string.lost_connection,
+					Toast.LENGTH_SHORT).show();
+			Log.d(DEBUG_TAG, "Devices has another connnection, refeshDisplay => false");
+		}
+	}
+
+}
